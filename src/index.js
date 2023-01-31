879350c9ba3506a5c3ef3992923d6adf2577bee5
arzafran <franco@studiofreight.com>
2023-01-31 16:47:06 -0300

    fix types

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-export { ReactLenis, useLenis, useScroll } from './lenis'
+import { useFrame } from '@studio-freight/hamo'
+import Lenis from '@studio-freight/lenis'
+import React, { createContext, useContext, useEffect, useRef, useState } from 'react'
+
+let rootLenisInstance
+
+export function useScroll(callback, root = false) {
+  const lenis = useLenis(root)
+
+  useEffect(() => {
+    if (!lenis) return
+
+    const unsubscribe = lenis.on('scroll', callback)
+    lenis.emit()
+
+    return unsubscribe
+  }, [lenis, callback])
+}
+
+export function useLenis(root = false) {
+  const lenis = useContext(LenisContext)
+
+  return root ? rootLenisInstance : lenis ?? rootLenisInstance
+}
+
+const LenisContext = createContext()
+
+export function ReactLenis({ children, root, ...options }) {
+  const wrapper = useRef()
+  const content = useRef()
+
+  const [lenis, setLenis] = useState()
+
+  useEffect(() => {
+    if (!root) {
+      options.wrapper = wrapper.current
+      options.content = content.current
+    }
+
+    const lenis = new Lenis({
+      ...options,
+    })
+
+    setLenis(lenis)
+
+    if (root) {
+      rootLenisInstance = lenis
+    }
+  }, [root])
+
+  useFrame((time) => {
+    lenis.raf(time)
+  }, [])
+
+  return (
+    <LenisContext.Provider value={lenis}>
+      {root ? (
+        children
+      ) : (
+        <div ref={wrapper}>
+          <div ref={content}>{children}</div>
+        </div>
+      )}
+    </LenisContext.Provider>
+  )
+}
