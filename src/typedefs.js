f6146648d36ff44e270123cc5edf57077896a117
Balraj <balraj.johal@phntms.com>
2023-08-10 22:46:25 +0100

    Add typedefs for dimensions and emitter  proos

--- src/typedefs.js
+++ src/typedefs.js
@@ -8,9 +8,15 @@
  * @typedef {Object} EventParams
  * TODO: this
  */
+
 /**
  * @callback CallbackFunction
  * @param {EventParams} [eventParams]
+ */
+
+/**
+ * @typedef {Object} CallbackEvents
+ * @property {CallbackFunction[]} scroll scroll events
  */
 
 /**
@@ -39,9 +45,9 @@
 /**
  * @typedef {Object} ReactLenisOptions
  * @property {(HTMLElement|Window)} [wrapper=window] interpolation rate
- * @property {HTMLElement=} [content=document.documentElement] interpolation rate
- * @property {(HTMLElement|Window)} [wheelEventsTarget=wrapper] interpolation rate
- * @property {number=} [lerp=0.1] interpolation rate
+ * @property {HTMLElement=} [content=document.documentElement] 
+ * @property {(HTMLElement|Window)} [wheelEventsTarget=wrapper] 
+ * @property {number=} [lerp=0.1] 
  * @property {number=} [duration=1.2] scroll duration
  * @property {EasingFunction=} [easing=(t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))] // easing function to apply to scroll values
  * @property {string=} [orientation='vertical'] scroll orientation
@@ -58,12 +64,21 @@
 
 /**
  * @typedef {Object} Dimensions
- * TODO: fill this
+ * @property {(HTMLElement|Window)} wrapper interpolation rate
+ * @property {HTMLElement} content interpolation rate
+ * @property {ResizeObserver} contentResizeObserver
+ * @property {function():void} resize 
+ * @property {function():void} onContentResize 
+ * @property {function():void} onWrapperResize 
+ * @property {number} width
+ * @property {number} height
+ * @property {number} scrollWidth
+ * @property {number} scrollHeight
  */
 
 /**
  * @typedef {Object} Emitter
- * TODO: fill this
+ * @property {CallbackEvents} events
  */
 
 /**
